e52c929519f0207db1b10074d2b2bf2c7556e9db
WincerChan <WincerChan@jxnu.edu.cn>
2017-07-22 10:22:19 +0800

    fix exec path from shell

--- init.el
+++ init.el
@@ -315,7 +315,6 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq exec-path-from-shell-check-startup-files nil)
   (setq configuration-layer--elpa-archives
         '(("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
           ("org-cn"   . "http://elpa.zilongshanren.com/org/")
